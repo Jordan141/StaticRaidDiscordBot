725a41f263df038f0b9fcfc965ebd83a52e1f21e
Jordan Moss <jmoss636@gmail.com>
2018-05-10 20:40:27 +0100

    More changes

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,45 @@
-const Discord = require('discord.io')
+const Discord = require('discord.js')
 const token = process.env.TOKEN || require('../auth.json').token
 const logger = require('winston')
+let staticRaid = []
+
+
+//Commands
+const REMOVE_MEMBER = '!removeme'
+const GET_MEMBERS = '!getmembers'
+
+//Responses
+const RAID_FULL = 'Current raid is full'
+const EMPTY_RAID = 'No members in current raid.'
+const ADDED_MEMBER = 'Added to static raid list.'
+const INVALID_USER = 'User is not part of raid.'
+const USER_ALREADY_EXISTS = 'You are already part of a raid!'
 
 logger.remove(logger.transports.Console)
 logger.add(logger.transports.Console, { colorize: true })
 logger.level = 'debug'
+logger.info(`Token: ${token}`)
+
+function addToRaid(message){
+    if(staticRaid.includes(message.author)) return USER_ALREADY_EXISTS
+    if(staticRaid.length >= 11) return RAID_FULL
+    
+    logger.info('addToRaid: ' + staticRaid.join(' ') + ' ' + message.author)
+    staticRaid.push(message.author)
+    return ADDED_MEMBER
+}
+
+function removeFromRaid(message){
+    const index = staticRaid.indexOf(message.author)
+    if(index === -1) return INVALID_USER
+    
+    staticRaid.splice(index, 1)
+    return 'Removed: ' + message.author
+}
+function raidToString(myRaidArray = []){
+    if(myRaidArray.length === 0) return EMPTY_RAID
+    return myRaidArray.reduce((sum, val, index) => sum += `#${index + 1} - ${val}`, '')
+}
 
 const bot = new Discord.Client({
     token: "",
@@ -17,18 +52,21 @@ bot.on('ready', () => {
     logger.info(`${bot.username} - # ${bot.id}`)
 })
  
-bot.on('message', (user, userID, channelID, message, event) => {
-   if(message[0] === '!'){
-       const args = message.substring(1).split(' ')
-       const cmd = args[0]
-       
-       switch(cmd){
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                })
-            break
-       }
-   }
+bot.on('message', message => {
+    const command = message.content.toLowerCase()
+
+    if(command === 'ping'){
+        message.reply('pong')
+    }
+    if(command === '+'){
+        message.reply(addToRaid(message))
+    }
+    if(command === REMOVE_MEMBER){
+        message.reply(removeFromRaid(message))
+    }
+    if(command === GET_MEMBERS){
+        message.reply(raidToString(staticRaid))
+    }
 })
+
+bot.login(token)
